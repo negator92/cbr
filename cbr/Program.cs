e8fe8a76b378be3d08a10ac930787aa62c2821ce
Vincent Neparidze <nva@akmetron.ru>
2018-09-12 16:00:41 +0300

    Refreshed to HttpClient, async and try catch

--- cbr/Program.cs
+++ cbr/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Globalization;
-using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
@@ -8,16 +10,31 @@ namespace cbr
 {
     internal class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            using (WebClient webClient = new WebClient())
+            if (args == null || args.Length > 1)
+                args = new string[] { "R01235" };
+            GetCurrency(args[0]).Wait();
+        }
+
+        public static async Task GetCurrency(string currency)
+        {
+            try
             {
-                string dateName = DateTime.Now.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
-                string xmlString = webClient.DownloadString($"https://www.cbr.ru/scripts/XML_daily_eng.asp?date_req={dateName}");
-                XDocument xDocument = XDocument.Parse(xmlString);
-                XElement xElement = xDocument.XPathSelectElement("//*[@ID=\"R01235\"]");
-                xElement = xElement.XPathSelectElement("Value");
-                Console.WriteLine(xElement.Value);
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    string address = $"https://www.cbr.ru/scripts/XML_daily_eng.asp?date_req={DateTime.Today.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)}";
+                    byte[] bytes = await httpClient.GetByteArrayAsync(address);
+                    string xmlStringUTF = Encoding.UTF8.GetString(bytes);
+                    XDocument xDocument = XDocument.Parse(xmlStringUTF);
+                    XElement xElement = xDocument.XPathSelectElement($"//*[@ID=\"{currency}\"]");
+                    xElement = xElement.XPathSelectElement("Value");
+                    Console.WriteLine(xElement.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
